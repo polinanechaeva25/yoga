7a8800594fd7f0a28fde859211195d3faafd5b24
Polina Nechaeva <polina2000_21@mail.ru>
2022-10-11 19:20:31 +0300

    update page contacts and add page about

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 # from django.conf.urls import include
 from django.conf.urls.static import static
 
-from mainapp.views import OrderListView
+from mainapp.views import MainListView, ContactListView, AboutListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', OrderListView.as_view(), name='index'),
+    path('', MainListView.as_view(), name='index'),
+    path('contact/', ContactListView.as_view(), name='contact'),
+    path('about/', AboutListView.as_view(), name='about'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,19 @@ from django.views.generic import ListView
 # from django.shortcuts import render
 
 
-class OrderListView(ListView):
-    title = 'список заказов'
+class MainListView(ListView):
+    title = 'YogaHanna'
     template_name = 'mainapp/index.html'
+    model = User
+
+
+class ContactListView(ListView):
+    title = 'Контакты - YogaHanna'
+    template_name = 'mainapp/contact.html'
+    model = User
+
+
+class AboutListView(ListView):
+    title = 'Обо мне - YogaHanna'
+    template_name = 'mainapp/about.html'
     model = User
