9eb2bbb6567b41ab2f8e2b5b39922e777a5dc807
Polina Nechaeva <polina2000_21@mail.ru>
2022-10-13 21:41:22 +0300

    were created pages about training

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 # from django.conf.urls import include
 from django.conf.urls.static import static
 
-from mainapp.views import MainListView, ContactListView, AboutListView
+from mainapp.views import MainListView, ContactListView, AboutListView, TrainingListView, TrainOffListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MainListView.as_view(), name='index'),
     path('contact/', ContactListView.as_view(), name='contact'),
     path('about/', AboutListView.as_view(), name='about'),
+    path('training/', TrainingListView.as_view(), name='training'),
+    path('training/offline/', TrainOffListView.as_view(), name='offline'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,19 +3,44 @@ from django.views.generic import ListView
 # from django.shortcuts import render
 
 
-class MainListView(ListView):
+class TitleContextMixin:
+
+    def get_title(self):
+        return getattr(self, 'title', '')
+
+    def get_context_data(self, **kwargs):
+        context = super(TitleContextMixin, self).get_context_data(**kwargs)
+        context.update(
+            title=self.get_title()
+        )
+        return context
+
+
+class MainListView(TitleContextMixin, ListView):
     title = 'YogaHanna'
     template_name = 'mainapp/index.html'
     model = User
 
 
-class ContactListView(ListView):
+class ContactListView(TitleContextMixin, ListView):
     title = 'Контакты - YogaHanna'
     template_name = 'mainapp/contact.html'
     model = User
 
 
-class AboutListView(ListView):
+class AboutListView(TitleContextMixin, ListView):
     title = 'Обо мне - YogaHanna'
     template_name = 'mainapp/about.html'
+    model = User
+
+
+class TrainingListView(TitleContextMixin, ListView):
+    title = 'Тренировки - YogaHanna'
+    template_name = 'mainapp/training.html'
+    model = User
+
+
+class TrainOffListView(TitleContextMixin, ListView):
+    title = 'Оффлайн занятия - YogaHanna'
+    template_name = 'mainapp/offline.html'
     model = User
